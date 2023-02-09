0f5910b2d3bf0629451dc022a28cfb51ba7749a9
MiguelCid <miguelcidyevenes@gmail.com>
2023-02-09 20:31:26 -0300

    primeros pasos de XMLHttpRequuest

---
+++ index.js
@@ -0,0 +1,9 @@
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+const xhr = new XMLHttpRequest();
+
+function onRequestHandler () {
+    if(this.readyState === 4 && this.status === 200) {
+        
+    }
+}
